b5c23eaf7b4bcc7fb415d390ad6def3e2daf3080
ZajcevDima <zaycevdi@gmail.com>
2025-10-13 18:47:29 +0300

    переделал вывод по заданию

--- 2-operations/discount.py
+++ 2-operations/discount.py
@@ -1,4 +1,5 @@
 price_of_goods = float(input("Напиши цену товара: "))
 discount = float(input("Напиши процент скидки: "))
 discount_price = price_of_goods * (discount / 100)
-print("Ваша скидка " + str(discount_price))
+
+print("Цена со скидкой " + str(price_of_goods - discount_price))
