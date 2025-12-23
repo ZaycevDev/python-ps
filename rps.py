7d34bfb627fe98ebe343eceb9cd765b4ebfba837
ZajcevDima <zaycevdi@gmail.com>
2025-12-24 00:31:24 +0300

    функции

--- rps.py
+++ rps.py
@@ -1,36 +1,63 @@
 import random
 
 CHOICE_RPS = ("камень", "ножницы", "бумага")
-print("Привет! Давай сыграем в игру камень, ножницы, бумага.")
-count_round = int(input("Сколько раундов со мной хочешь сыграть?: "))
-user_points = 0
-bot_points = 0
 
-for round in range(count_round):
-    print(f"Раунд № {round + 1}")
-    user = input("Введите камень/ножницы/бумага: ").lower()
 
-    while user not in CHOICE_RPS:
+def input_user(is_replay: bool) -> str:
+    return input(f"Введите {"снова " if is_replay else ""}камень/ножницы/бумага: ").lower()
+
+
+def user_selects_option():
+    user_select = input_user(False)
+
+    while user_select not in CHOICE_RPS:
         print("Не корректный выбор")
-        user = input("Введите снова камень/ножницы/бумага: ").lower()
+        user_select = input_user(True)
 
-    bot = random.choice(CHOICE_RPS)
-    print(f"Я выбрал: {bot}")
+    return user_select
 
-    if user == bot:
-        print("В этом раунде ничья")
-    elif (user == "камень" and bot == "ножницы") or\
-        (user == "ножницы" and bot == "бумага") or\
-            (user == "бумага" and bot == "камень"):
-        print("В этом раунде победа за вами!")
-        user_points += 1
+
+def rounds_game(count: int):
+    user_points = 0
+    comp_points = 0
+
+    for round in range(count):
+        print(f"Раунд № {round + 1}")
+
+        user_select = user_selects_option()
+        comp = random.choice(CHOICE_RPS)
+        print(f"Я выбрал: {comp}")
+
+        if user_select == comp:
+            print("В этом раунде ничья")
+        elif (user_select == "камень" and comp == "ножницы") or\
+            (user_select == "ножницы" and comp == "бумага") or\
+                (user_select == "бумага" and comp == "камень"):
+            print("В этом раунде победа за вами!")
+            user_points += 1
+        else:
+            print("В этом раунде победа за мной")
+            comp_points += 1
+
+    return (user_points, comp_points)
+
+
+def Game():
+    print("Привет! Давай сыграем в игру камень, ножницы, бумага.")
+
+    count_round = "0"
+
+    while not count_round.isnumeric() or int(count_round) < 1:
+        count_round = input("Сколько раундов со мной хочешь сыграть?: ")
+
+    (user_points, comp_points) = rounds_game(int(count_round))
+
+    if user_points > comp_points:
+        print("По итогам всех раундов Вы выиграли.")
+    elif user_points == comp_points:
+        print("По итогам всех раундов ничья.")
     else:
-        print("В этом раунде победа за мной")
-        bot_points += 1
+        print("По итогам всех раундов Вы проиграли")
 
-if user_points > bot_points:
-    print("По итогам всех раундов Вы выиграли.")
-elif user_points == bot_points:
-    print("По итогам всех раундов ничья.")
-else:
-    print("По итогам всех раундов Вы проиграли")
+
+Game()
